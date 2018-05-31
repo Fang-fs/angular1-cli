51f966801b914981a76a9795814cfd72aca7deba
Fang-fs <shenzhufang@163.com>
2018-05-31 14:21:37 +0800

    two commit

--- src/static/directive/checkListBox/index.js
+++ src/static/directive/checkListBox/index.js
@@ -1,9 +1,7 @@
 import './index.less';
 import template from './index.html';
 
-/**
- * 左侧导航nav
- */
+
 vapour.directive('checkListBox', ($rootScope, $location) => {
     return {
         template: template,
